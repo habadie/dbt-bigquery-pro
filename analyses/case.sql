57b5074ec856a1f2ce56b773940ac99a13067748
habadie <habadi.pro@gmail.com>
2026-07-31 18:51:11 +0200

    test coomplie

---
+++ analyses/case.sql
@@ -0,0 +1,6 @@
+CASE a 
+       WHEN 1 THEN 'un'
+       WHEN 2 THEN 'deux'
+       WHEN 3 THEN 'trois'
+       ELSE 'autre'
+END
---
+++ macros/session_1.sql
@@ -0,0 +1,2 @@
+{% set mon_nom = 'Alice' %}
+select '{{ mon_nom }}' as prenom
